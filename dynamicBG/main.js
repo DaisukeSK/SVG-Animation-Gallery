b8cbbb0dac64fc8ed340e8644e0252867590ae20
Daisuke <skd.itkn@gmail.com>
2023-12-28 20:26:19 -0800

    cmt

--- dynamicBG/main.js
+++ dynamicBG/main.js
@@ -1,16 +1,14 @@
 
 const bg1=document.querySelector(".bg1")
 const bg2=document.querySelector(".bg2")
-
+const input=document.querySelector('input[type="range"]')
 
 document.onmousemove=(e) => {
-    // console.log("-------------------------")
-    // console.log(e.clientY/window.innerHeight) // THIS should do what you want
-    // console.log(e.clientX/window.innerWidth)
-    // console.log("bg1",bg2.getBoundingClientRect().height-window.innerHeight)
-
-
-
     bg1.style.left=-(bg1.getBoundingClientRect().width-window.innerWidth-100)*e.clientX/window.innerWidth+"px"
     bg2.style.top=-(bg2.getBoundingClientRect().height-window.innerHeight-100)*e.clientY/window.innerHeight+"px"
+}
+
+input.oninput=(e)=>{
+    bg1.style.transition=`all ${3-e.target.value*3/10}s ease-in-out`;
+    bg2.style.transition=`all ${3-e.target.value*3/10}s ease-in-out`;
 }
--- shapesNcolors/main.js
+++ shapesNcolors/main.js
@@ -1,183 +1,159 @@
 
-const colorsArray=[
+const colorsArray =[
     {1:"cyan", 2:"lightblue", 3:"blue", bg:'rgba(0, 0, 255, 0.5)'},
     {1:"orange", 2:"red", 3:"yellow", bg:'rgba(255, 0, 0, 0.5)'},
     {1:"green", 2:"lightgreen", 3:"chartreuse", bg:'rgba(0, 128, 0, 0.5)'}
-]
+];
 
-const styleTransform=[
+const styleTransform =[
     "translateY(-50%)",
     "translateY(-50%) scale(.7, .7)",
     "translateY(-50%) scale(.7, .7) rotate(90deg)"
-]
+];
 
-let type=0
-let color=0
+let type=0;
+let color=0;
 
-const colorButtons=document.querySelectorAll(".color1, .color2, .color3")
-const shapeButtons=document.querySelectorAll(".shape1, .shape2, .shape3")
+const boxShadowStyle="0px 0px 5px 3px #ffffff";
+const outlineStyle='3px solid white';
 
-colorButtons[0].style.pointerEvents="none"
-colorButtons[0].style.outline='3px solid white'
-colorButtons[0].style.boxShadow= "0px 0px 5px 3px #ffffff";
-
-shapeButtons[0].style.pointerEvents="none"
-shapeButtons[0].style.outline='3px solid white'
-shapeButtons[0].style.boxShadow= "0px 0px 5px 3px #ffffff";
-
-shapeButtons[0].querySelector("circle").setAttribute("fill",colorsArray[0]["bg"])
-shapeButtons[1].querySelector("rect").setAttribute("fill",colorsArray[0]["bg"])
-shapeButtons[2].querySelector("path").setAttribute("stroke",colorsArray[0]["bg"])
-
-
-colorButtons.forEach((val,key)=>{
-val.querySelector("circle").setAttribute("fill",colorsArray[key]["bg"])
-
-})
+const colorButtons=document.querySelectorAll(".color1, .color2, .color3");
+const shapeButtons=document.querySelectorAll(".shape1, .shape2, .shape3");
 
 const typeChange=(num, color, width)=>{
 
-    if(num==0){
-        return `<circle cx="50%" cy="50%" r="50%" fill=${color} stroke="none" filter="blur(7px)">`
-    }else if(num==1){
-        return `<rect x="0" y="0" width="100%" height="100%" fill=${color} stroke="none">`
-    }else if(num==2){
-        return `<path d='
-        M${width*0.732/3.464} 0
-        l${width*2/3.464} 0
-        l${width*1/3.464} ${width/2}
-        l-${width*1/3.464} ${width/2}
-        l-${width*2/3.464} 0
-        l-${width*1/3.464} -${width/2}
-        l${width*1/3.464} -${width/2}
-        l${width*2/3.464} 0'
-        fill="none" stroke=${color} stroke-width="30">`
+    switch(num){
+        case 0: return `<circle cx="50%" cy="50%" r="50%" fill=${color} stroke="none" filter="blur(7px)">`;
+        case 1: return `<rect x="0" y="0" width="100%" height="100%" fill=${color} stroke="none">`;
+        default : return`<path d='
+            M${width*0.732/3.464} 0
+            l${width*2/3.464} 0
+            l${width*1/3.464} ${width/2}
+            l-${width*1/3.464} ${width/2}
+            l-${width*2/3.464} 0
+            l-${width*1/3.464} -${width/2}
+            l${width*1/3.464} -${width/2}
+            l${width*2/3.464} 0'
+            fill="none" stroke=${color} stroke-width="30">`;
     }
-
-}
+};
 
 const makeCircle=()=>{
-    const xx=Math.random()*100
-    const yy=Math.random()*100
-    const glow=Math.random()*5+5
-    const delay=Math.random()*5
-    const diameter=Math.random()*150+100
-
-    const rand=Math.ceil(Math.random()*3)
-
-    // let color= rand<1? "cyan": 1<=rand && rand<2? "lightblue":"lightgreen"
+    const xx=Math.random()*100;
+    const yy=Math.random()*100;
+    const glow=Math.random()*5+5;
+    const delay=Math.random()*5;
+    const diameter=Math.random()*150+100;
+    const rand=Math.ceil(Math.random()*3);
 
     const svg= 
-
     `<svg width=${diameter} height=${diameter}
-    style="top:${xx}%; left:${yy}%; animation-duration: ${glow}s; animation-delay: ${delay}s;
-    transform:${styleTransform[type]};">`+
+        style="top:${xx}%; left:${yy}%; animation-duration: ${glow}s; animation-delay: ${delay}s;
+        transform:${styleTransform[type]};">`+
 
-    typeChange(type, colorsArray[color][rand], diameter)+
-    
-    '</svg>'
-    document.querySelector(".circle").insertAdjacentHTML("afterbegin", svg)
-    document.querySelector(".circle svg").onanimationend=(e)=>{e.target.remove()}
-}
+        typeChange(type, colorsArray[color][rand], diameter)+
 
-const bgChange=(color)=>{
-    document.querySelector("body").style.background=`linear-gradient(${colorsArray[color][1]},${colorsArray[color][2]},${colorsArray[color][3]})`
-}
+    '</svg>';
 
-window.onload=()=>{
-    bgChange(color)
-    
-    for(let i=0; i<=20; i++){
+    document.querySelector(".circle").insertAdjacentHTML("afterbegin", svg);
+    document.querySelector(".circle svg").onanimationend=(e)=>e.target.remove();
+};
 
-        makeCircle()
-    }
-    document.querySelectorAll(".circle svg").forEach(val=>{
-        val.style.animationIterationCount=4;
-        
-    })
-
-}
-
-
-// console.log("///////////// Start /////////////")
-setInterval(()=>{
-    makeCircle()
-    // console.log(document.querySelectorAll(".circle svg").length)
-},2000)
+const bgChange=(number)=>{
+    document.querySelector("body").style.background=`linear-gradient(${colorsArray[number][1]},${colorsArray[number][2]},${colorsArray[number][3]})`
+};
 
 
 colorButtons.forEach((val,key)=>{
     val.onclick=(e)=>{
-        color=key
+        color=key;
 
         colorButtons.forEach((val2,key)=>{
-            val2.style.outline='none'
-            val2.style.boxShadow='none'
+            val2.style.outline='none';
+            val2.style.boxShadow='none';
             val2.style.pointerEvents= 'auto';
         })
 
-        e.target.closest("div").style.outline='3px solid white'
-        e.target.closest("div").style.boxShadow= "0px 0px 5px 3px #ffffff";
+        e.target.closest("div").style.outline=outlineStyle;
+        e.target.closest("div").style.boxShadow= boxShadowStyle;
         e.target.closest("div").style.pointerEvents= 'none';
 
-        shapeButtons[0].querySelector("circle").setAttribute("fill",colorsArray[key]["bg"])
-        shapeButtons[1].querySelector("rect").setAttribute("fill",colorsArray[key]["bg"])
-        shapeButtons[2].querySelector("path").setAttribute("stroke",colorsArray[key]["bg"])
+        shapeButtons[0].querySelector("circle").setAttribute("fill",colorsArray[key]["bg"]);
+        shapeButtons[1].querySelector("rect").setAttribute("fill",colorsArray[key]["bg"]);
+        shapeButtons[2].querySelector("path").setAttribute("stroke",colorsArray[key]["bg"]);
 
-
-        console.log("Clicked")
-        bgChange(color)
+        bgChange(color);
         
         document.querySelectorAll(".circle svg").forEach(svg=>{
             const rand=Math.ceil(Math.random()*3)
             
-            if(svg.firstChild.tagName=="path"){
-                svg.firstChild.setAttribute("stroke",colorsArray[key][rand])
-            }else{
+            svg.firstChild.tagName=="path"?
+            svg.firstChild.setAttribute("stroke",colorsArray[key][rand]):
+            svg.firstChild.setAttribute("fill",colorsArray[key][rand])
 
-                svg.firstChild.setAttribute("fill",colorsArray[key][rand])
-            }
-
-        })
-    
-    }
-})
+        });
+    };
+});
 
 
 shapeButtons.forEach((val,key)=>{
 
-
     val.onclick=(e)=>{
-        type=key
-        console.log("Clicked",val.classList[0])
-
-        shapeButtons.forEach((val2,key)=>{
-            val2.style.outline='none'
-            val2.style.boxShadow='none'
-            val2.style.pointerEvents= 'auto';
-        })
-
-
-        e.target.closest("div").style.outline='3px solid white'
-        e.target.closest("div").style.boxShadow= "0px 0px 5px 3px #ffffff";
-        e.target.closest("div").style.pointerEvents= 'none';
- 
+        type=key;
         
-        // console.log("SVg",val)
+        shapeButtons.forEach((val2,key)=>{
+            val2.style.outline='none';
+            val2.style.boxShadow='none';
+            val2.style.pointerEvents= 'auto';
+        });
+
+        e.target.closest("div").style.outline=outlineStyle;
+        e.target.closest("div").style.boxShadow= boxShadowStyle;
+        e.target.closest("div").style.pointerEvents= 'none';
+        
         document.querySelectorAll(".circle svg").forEach(svg=>{
-            console.log("Size",typeof Math.round(svg.getAttribute("width")),Math.round(svg.getAttribute("width")),Math.round(svg.getAttribute("height")))
             
-            svg.style.transform=styleTransform[key]
+            svg.style.transform=styleTransform[key];
+            let fill=svg.firstChild.getAttribute("fill")!=="none"?
+                svg.firstChild.getAttribute("fill"):
+                svg.firstChild.getAttribute("stroke");
+            svg.firstChild.remove();
+            svg.insertAdjacentHTML("beforeend",typeChange(type, fill, Math.round(svg.getAttribute("width"))));
+            
+        });
+    };
+});
 
-            console.log("val",svg.firstChild.tagName)
-           
-            let fill=svg.firstChild.getAttribute("fill")!=="none"?svg.firstChild.getAttribute("fill"):svg.firstChild.getAttribute("stroke")
-            console.log("fill",fill)
-            svg.firstChild.remove()
-            // svg.insertAdjacentHTML("beforeend",`<rect x="0" y="0" width="60%" height="60%" fill=${fill} stroke="none">`)
-            svg.insertAdjacentHTML("beforeend",typeChange(type, fill, Math.round(svg.getAttribute("width"))))
+
+window.onload=()=>{
+
+    colorButtons[0].style.pointerEvents="none";
+    colorButtons[0].style.outline=outlineStyle;
+    colorButtons[0].style.boxShadow= boxShadowStyle;
+
+    shapeButtons[0].style.pointerEvents="none";
+    shapeButtons[0].style.outline=outlineStyle;
+    shapeButtons[0].style.boxShadow= boxShadowStyle;
+
+    shapeButtons[0].querySelector("circle").setAttribute("fill",colorsArray[0]["bg"]);
+    shapeButtons[1].querySelector("rect").setAttribute("fill",colorsArray[0]["bg"]);
+    shapeButtons[2].querySelector("path").setAttribute("stroke",colorsArray[0]["bg"]);
+
+    colorButtons.forEach((val,key)=>{
+        val.querySelector("circle").setAttribute("fill",colorsArray[key]["bg"]);
+    });
+
+    bgChange(color);
     
-        })
-    }
+    for(let i=0; i<=20; i++){
+        makeCircle();
+    };
 
-})
+    document.querySelectorAll(".circle svg").forEach(val=>{
+        val.style.animationIterationCount=4;
+    });
+
+    setInterval(()=>{
+        makeCircle();
+    },2000);
+};
