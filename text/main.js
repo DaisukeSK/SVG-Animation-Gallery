bf0bb496ad51cd7c05a07f3dadc0cb4009b7ed35
Daisuke <skd.itkn@gmail.com>
2024-03-15 21:34:31 -0700

    text type new type added

--- text/main.js
+++ text/main.js
@@ -5,10 +5,18 @@ const initText = document.querySelector(".initText")
 const zoomOut = document.querySelector(".zoomOut")
 const opacityAnimate = document.querySelector(".opacityAnimate")
 const blackGradAnm = document.querySelectorAll(".blackGradAnm")
+const svg = document.querySelector(".outputDiv svg")
+const translate = document.querySelectorAll(".translate")
+const skew = document.querySelectorAll(".skew")
 
 initText.style.transform=`translateX(-${initText.getBoundingClientRect().width/2}px)`
 
 Input.oninput=(e)=>{
+  // console.log("text width:",outputTXT[0].getBoundingClientRect().width)
+  // console.log("svg:",svg)
+  // svg.style.width=outputTXT[0].getBoundingClientRect().width+"px"
+  svg.setAttribute("width",outputTXT[0].getBoundingClientRect().width+100)
+
   e.target.value=="" ?
   initText.setAttribute("display","block"):
   initText.setAttribute("display","none")
@@ -17,16 +25,36 @@ Input.oninput=(e)=>{
   );
 }
 
+
 typeButtons.forEach((bttn,key)=>{
   bttn.onclick=()=>{
+    outputTXT[0].setAttribute("fill","none")
+    outputTXT[0].setAttribute("stroke-width","9")
+    // outputTXT[1].setAttribute("stroke-width","3")
+
+    // outputTXT[0].setAttribute("style","font-weight: 900;font-style: italic;font-family: arial;font-size: 5em;")
+    // outputTXT[1].setAttribute("style","font-weight: 900;font-style: italic;font-family: arial;font-size: 5em;")
+
+    outputTXT[0].setAttribute("font-family","arial")
+    outputTXT[1].setAttribute("font-family","arial")
+
+    translate[0].removeAttribute("values")
+    translate[1].removeAttribute("values")
+
+    skew[0].removeAttribute("values")
+    skew[1].removeAttribute("values")
+
+    outputTXT[0].setAttribute("font-style","italic")
+    outputTXT[1].setAttribute("font-style","italic")
+
     if(key==0){
       outputTXT[0].setAttribute("stroke","LightSlateGrey")
-      outputTXT[0].setAttribute("fill","none")
+      // outputTXT[0].setAttribute("fill","none")
       outputTXT[1].setAttribute("stroke","url(#type1_1)")
       outputTXT[1].setAttribute("fill","url(#type1_2)")
     }else if(key==1){
       outputTXT[0].setAttribute("stroke","navy")
-      outputTXT[0].setAttribute("fill","none")
+      // outputTXT[0].setAttribute("fill","none")
       outputTXT[1].setAttribute("stroke","white")
       outputTXT[1].setAttribute("fill","url(#type2_1)")
     }else if(key==2){
@@ -40,7 +68,7 @@ typeButtons.forEach((bttn,key)=>{
       opacityAnimate.setAttribute("values","0;1" );
       opacityAnimate.beginElement()
       outputTXT[0].setAttribute("stroke","#cccccc")
-      outputTXT[0].setAttribute("fill","none")
+      // outputTXT[0].setAttribute("fill","none")
       outputTXT[1].setAttribute("stroke","url(#type4_2)")
       outputTXT[1].setAttribute("fill","url(#type4_1)")
       setTimeout(()=>{
@@ -49,6 +77,40 @@ typeButtons.forEach((bttn,key)=>{
           val.beginElement()
         })
       },1000)
+    }else if(key==4){
+      outputTXT[0].setAttribute("stroke-width","3")
+      outputTXT[0].setAttribute("stroke","grey")
+
+// outputTXT[0].setAttribute("style","font-weight: 900;font-style: italic;font-family: Times New Roman;font-size: 5em;")
+//     outputTXT[1].setAttribute("style","font-weight: 900;font-style: italic;font-family: Times New Roman;font-size: 5em;")
+    outputTXT[0].setAttribute("font-family","Times New Roman")
+    outputTXT[1].setAttribute("font-family","Times New Roman")
+
+      // outputTXT[0].setAttribute("fill","none")
+      outputTXT[1].setAttribute("stroke","none")
+      outputTXT[1].setAttribute("fill","url(#type5_1)")
+
+      
+    }else if(key==5){
+      translate[0].setAttribute("values","60;-60;60")
+      translate[1].setAttribute("values","60;-60;60")
+
+      skew[0].setAttribute("values","-25;25;-25")
+      skew[1].setAttribute("values","-25;25;-25")
+
+      outputTXT[0].removeAttribute("font-style")
+      outputTXT[1].removeAttribute("font-style")
+
+      outputTXT[0].setAttribute("stroke","none")
+      outputTXT[1].setAttribute("stroke","none")
+
+      outputTXT[0].setAttribute("font-family","Times New Roman")
+    outputTXT[1].setAttribute("font-family","Times New Roman")
+
+      outputTXT[0].setAttribute("fill","url(#type6_1)")
+      outputTXT[1].setAttribute("fill","url(#type6_2)")
+
+      
     }
   }
 })
